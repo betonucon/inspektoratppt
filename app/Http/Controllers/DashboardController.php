5806c1fe74c298b777ae358a0fafc37f0a210c48
bagus.dev <bagus.bip24@gmail.com>
2022-12-29 14:29:13 +0700

    push

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lhp;
+use App\Models\Pkpt;
+use App\Models\ProgramKerja;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -9,6 +12,12 @@ class DashboardController extends Controller
     public function index()
     {
         $menu = 'Dashboard';
-        return view('dashboard.index', compact('menu'));
+        $belumTindakLanjut = Lhp::where('status', 'BT')->count();
+        $tidakDapatDitindaklanjuti = Lhp::where('status', 'TT')->count();
+        $sesuai = Lhp::where('status', 'S')->count();
+        $belumSesuai = Lhp::where('status', 'BS')->count();
+
+
+        return view('dashboard.index', compact('menu', 'belumTindakLanjut', 'tidakDapatDitindaklanjuti', 'sesuai', 'belumSesuai'));
     }
 }
